f63fa4e0eae86d56338e10e63f0fbb2a4f3ac45a
Hatef <letstan009@gmail.com>
2025-08-29 21:29:59 +0330

    edited

--- database.py
+++ database.py
@@ -1,25 +1,20 @@
 from pymongo import MongoClient, ASCENDING
 from datetime import datetime
 from config import MONGO_URI, DB_NAME
-from pymongo import MongoClient
-import os
 
-MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DB_NAME", "language_coach")
-
+# اتصال به دیتابیس
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
+
+# کالکشن‌ها
 users_col = db["users"]
 lessons_col = db["lessons"]
 
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
-
-
-# Indexes for performance (idempotent)
+# ایندکس‌ها برای بهبود عملکرد
 users_col.create_index([("user_id", ASCENDING)], unique=True)
 lessons_col.create_index([("user_id", ASCENDING), ("created_at", ASCENDING)])
 
+# فانکشن‌های کاربردی
 def get_user(user_id: int):
     return users_col.find_one({"user_id": user_id})
 
